54403c193ae8604f2b1e22bb684159d83c2c8a9f
c-brenn <brennc27@tcd.ie>
2015-02-04 19:49:06 +0000

    changed the way in which I measure the runtime of each subprocess, new results from running 'measure_time.py' on my laptop

--- lab1/measure_time.py
+++ lab1/measure_time.py
@@ -5,6 +5,7 @@ from subprocess import Popen, PIPE, STDOUT
 import multiprocessing as mp
 import math
 import json
+import time as t
 
 x = []
 y = []
@@ -15,18 +16,21 @@ p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
 	    stderr=STDOUT, close_fds=True)
 p.wait() #wait for compiling to complete
 
-samples = 4
+samples = 3
 
 max_threads = int(1.5 * mp.cpu_count())
 
 for i in range(1, max_threads):
-	cmd = "time -p ./leibniz " + str(i)
+	cmd = "./leibniz " + str(i)
 	time = 0
 	for j in range(samples):
+		start = t.time()
 		p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, 
 		    stderr=STDOUT, close_fds=True)
-		time = time + float(re.findall("\d+.\d+", p.stdout.readline())[0])
+		#time = time + float(re.findall("\d+.\d+", p.stdout.readline())[0])
 		p.wait()
+		end = t.time()
+		time = time + end - start
 	time = time / samples
 	x.append(i)
 	y.append(time)
