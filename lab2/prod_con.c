c3920f4c6cb3bc29e4a7636c8025a7d8e0a5113a
c-brenn <brennc27@tcd.ie>
2015-02-21 15:55:05 +0000

    redid lab 2 to make it nice. todo : add colours to prints, add readme

--- lab2/prod_con.c
+++ lab2/prod_con.c
@@ -1,147 +1,206 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
+#include "prod_con.h"
 
-typedef enum { FALSE, TRUE} boolean;
+pthread_mutex_t buffer_mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t full_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty_cond = PTHREAD_COND_INITIALIZER;
 
-typedef struct {
-	int n;
-	int size;
-	int num_to_produce;
-	boolean finished;
-}buffer;
+int items_to_create;
+int num_consumers;
+boolean finished_producing = FALSE;
 
-buffer * buffer_new(int size, int num_to_produce) {
-	buffer * this = malloc(sizeof(buffer));
-	this->n = 0;
-	this->num_to_produce = num_to_produce;
-	this->size = size;
-	this->finished = FALSE;
+item * item_new(char * d) {
+	item * this = malloc(sizeof(item));
+	this->data = malloc(strlen(d) + 1);
+	strcpy(this->data, d);
 	return this;
 }
 
-void check_results(char * string, int val){          
-	if(val) {
-		printf("Failed with %d at %s", val, string);
-		exit(1);
-	}                                       
+void item_free(item * this) {
+	free(this->data);
+	free(this);
 }
 
-void print_buffer(buffer * buff, char * s) {
-	fprintf(stdout, "%s ||", s);
+void item_print(item * this) {
+	printf("[%s]", this->data);
+	return;
+}
+
+void item_update_data(item * this, char * s) {
+	this->data = realloc(this->data, strlen(s) + 1);
+	strcpy(this->data, s);
+	return;
+}
+
+buffer * buffer_new(int size) {
+	buffer * this = malloc(sizeof(buffer));
+	this->size = size;
+	this->n = 0;
+	this->items = calloc(size, sizeof(item*));
+	return this;
+}
+
+void buffer_free (buffer * this) {
 	int i;
-	for(i = 0; i < buff->size; i++) {
-		if(i < buff->n) {
-			fprintf(stdout, "[]");
-		}
-		if(i < buff->size - 1) {
-			fprintf(stdout, ", ");
-		}
+	for(i = 0; i < this->size; i++) {
+		item_free(this->items[i]);
 	}
-	fprintf(stdout, "||\n");
+	free(this->items);
+	free(this);
+	return;
 }
 
-void do_work() {
-	float t = (float)rand() / (float) RAND_MAX;
-	t = 5 * t;
-	sleep((int) t);
-
+void buffer_add_item (buffer * this, char * s) {
+	if(this->items[this->n] == NULL) {
+		this->items[this->n++] = item_new(s);
+	} else {
+		item_update_data(this->items[this->n++], s);
+	}
+	return;
 }
 
-boolean full = FALSE;
-boolean empty  = TRUE;
-pthread_mutex_t buff_mutex =  PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t producer_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t consumer_cond = PTHREAD_COND_INITIALIZER;
+void buffer_remove_item(buffer * this) {
+	this->n--;
+}
 
-void * produce(void* buff) {
-	buffer * b = (buffer*) buff;
-	int rc = pthread_mutex_lock(&buff_mutex);
-	check_results("pthread_mutex_lock() producer\n", rc);
-	while(!b->finished) {
-		while(full && !b->finished) {
-			rc = pthread_cond_wait(&producer_cond, &buff_mutex);
-			if (rc) {
-				printf("Producer condwait failed, rc=%d\n", rc);
-				pthread_mutex_unlock(&buff_mutex);
-				exit(1);
-			}
+void buffer_print(buffer * this) {
+	system("clear");
+	printf("\n\t");
+	int i;
+	for (i = 0; i < this->size; i++) {
+		
+		if (i % ROW_SIZE == 0) {
+			printf("\n\t");
 		}
-		if(b->num_to_produce-- == 0) {
-			b->finished = TRUE;
-			printf("Producer has finished producing.\n");
+		if (i < this->n) {
+			item_print(this->items[i]);
 		} else {
-			b->n++;
-			print_buffer(b, "Produce");
-			empty = FALSE;
-			if(b->n == b->size) {
-				full = TRUE;
-			}
+			printf("[    ]");
 		}
-		rc = pthread_mutex_unlock(&buff_mutex);
-		check_results("Producer mutex_unlock()\n", rc);
-		rc = pthread_cond_signal(&consumer_cond);
-		do_work();
 	}
-	printf("Producer exit.\n");
+	printf("\n");
+	return;
+}
+
+boolean buffer_is_full(buffer * this) {
+	return this->n == this->size;
+}
+
+boolean buffer_is_empty(buffer * this) {
+	return this->n == 0;
+}
+
+void timer(long i) {
+	int t = i;
+	while(t > 0) {
+		t--;
+	}
+	return;
+}
+
+void * produce(void * args) {
+	buffer * buff = (buffer*) args;
+	int items_left = items_to_create;
+	while(items_left > 0) {
+		// simulate creating item
+		char* data = "item";
+		timer(PRODUCE_TIME);
+
+		pthread_mutex_lock(&buffer_mut);
+		if (!buffer_is_full(buff)) {
+			// add an item to the buffer
+			buffer_add_item(buff, data);
+			buffer_print(buff);
+			items_left--;
+			printf("items left: %d\n", items_left);
+			// release the mutex and wake tell consumers that
+			// the buffer isn't empty
+			pthread_mutex_unlock(&buffer_mut);
+			pthread_cond_broadcast(&empty_cond);
+		} else {
+			// wait for empty slot in buffer
+			pthread_mutex_unlock(&buffer_mut);
+			pthread_cond_wait(&full_cond, &buffer_mut);
+		}
+	}
+	pthread_mutex_unlock(&buffer_mut);
+	finished_producing = TRUE;
 	pthread_exit(0);
 }
 
-void * consume(void* buff) {
-	buffer * b = (buffer*) buff;
-	int rc = pthread_mutex_lock(&buff_mutex);
-	check_results("pthread_mutex_lock() consume\n", rc);
-	while(!b->finished || b->n > 0) {
-		while(empty && !b->finished) {
-			rc = pthread_cond_wait(&consumer_cond, &buff_mutex);
-			if (rc) {
-				printf("Consumer condwait failed, rc=%d\n", rc);
-				pthread_mutex_unlock(&buff_mutex);
-				exit(1);
-			}
+void * consume(void * args) {
+	buffer * buff = (buffer*) args;
+	
+	while(!finished_producing || !buffer_is_empty(buff)) {
+		
+		pthread_mutex_lock(&buffer_mut);
+		if(!buffer_is_empty(buff)) {
+			// remove and item from the buffer
+			buffer_remove_item(buff);
+			buffer_print(buff);
+			// release the mutex and tell the producer
+			// that the buffer isn't full
+			pthread_mutex_unlock(&buffer_mut);
+			pthread_cond_broadcast(&full_cond);
+			timer(CONSUME_TIME);
+		} else {
+			// wait for producer to place an item in the buffer
+			pthread_mutex_unlock(&buffer_mut);
+			pthread_cond_wait(&empty_cond, &buffer_mut);
 		}
-		if (b->n > 0) {
-			b->n--;
-			print_buffer(b, "Consume");
-			full = FALSE;
-			if(b->n == 0) {
-				empty = TRUE;
-			}
-		}
-		rc = pthread_mutex_unlock(&buff_mutex);
-		check_results("Consumer mutex_unlock()\n", rc);
-		rc = pthread_cond_signal(&producer_cond);
-		do_work();
+		pthread_mutex_unlock(&buffer_mut);
 	}
-	printf("Consumer exit.\n");
+	pthread_mutex_unlock(&buffer_mut);
 	pthread_exit(0);
 }
- 
 
+int main(int argc, char ** argv) {
+	// ensure that program was called with correct num of params
+	if (argc < 2) {
+		printf("Like this pls: ./pc <number of consumers>\n");
+		exit(1);
+	}
+	// check that params are sensible
+	num_consumers = atoi(argv[1]);
+	if (num_consumers < 1 || num_consumers > MAX_CONSUMERS) {
+		printf("That's a silly number of consumers\n");
+		exit(1);
+	}
+	// create the buffer
+	items_to_create = num_consumers * ITEMS_PER_CON;
+	buffer * b = buffer_new(BUFFER_SIZE);
+	
+	// create and start producer
+	pthread_t producer;
+	if (pthread_create(&producer, NULL, produce, (void*) b)) {
+		printf("Error creating producer thread.\n");
+		exit(1);
+	}
+	pthread_cond_signal(&full_cond);
 
-int main(int argc, char ** argv){
-	printf("Creating buffer\n");
-	buffer * buff = buffer_new(10, 10);
-	// parse num_threads from args
-	int num_consumers = atoi(argv[1]);
+	//create and start consumers
 	pthread_t consumers[num_consumers];
-	pthread_t producers[num_consumers];
-	printf("Creating producer...\n");
 	int i;
-	for(i = 0; i < num_consumers; i++) {
-		printf("Creating Producer: %d\n", i);
-		int return_code = pthread_create(&producers[i],NULL,
-									produce,(void *)buff);
-		check_results("pthread_create() consumer\n", return_code);
-		printf("Creating consumer: %d\n", i);
-		return_code = pthread_create(&consumers[i],NULL,
-									consume,(void *)buff);
-		check_results("pthread_create() producer\n", return_code);
+	for (i = 0; i < num_consumers; i++) {
+		printf("Created consumer: %d.\n", i);
+		if (pthread_create(&consumers[i], NULL, consume, (void*) b)) {
+			printf("Error creating consumer thread: %d.\n", i);
+			exit(1);
+		}
 	}
-	for(i = 0; i < num_consumers; i++) {
-		pthread_join(producers[i], NULL);
-		pthread_join(consumers[i], NULL);
+
+	// wait for producer to join
+	if (pthread_join(producer, NULL)){
+		printf("Error joining producer thread.\n");
+		exit(1);
 	}
-	print_buffer(buff, "Done: ");
-	return 0;	
+
+	// wait for consumers to join;
+	for (i = 0; i < num_consumers; i++) {
+		if (pthread_join(consumers[i], NULL)){
+		printf("Error joining consumer thread: %d.\n", i);
+		exit(1);
+	}
+	}
+
+	return 0;
 }
---
+++ lab2/prod_con.h
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+
+#define ITEMS_PER_CON 10
+#define BUFFER_SIZE 40
+#define ROW_SIZE 10
+#define MAX_CONSUMERS 100
+#define PRODUCE_TIME 100000000
+#define CONSUME_TIME 500000000
+
+typedef enum{
+	FALSE, TRUE
+}boolean;
+
+typedef struct{
+	char * data;
+}item;
+
+typedef struct{
+	int size;
+	int n;
+	item ** items;
+}buffer;
+
+item * item_new(char * d);
+void item_free(item * this);
+void item_print(item * this);
+void item_update_data(item * this, char * s);
+buffer * buffer_new(int size);
+void buffer_free(buffer * this);
+void buffer_add_item(buffer * this, char * s);
+void buffer_remove_item(buffer * this);
+void buffer_print(buffer * this);
+boolean buffer_is_full(buffer * this);
+boolean buffer_is_empty(buffer * this);
+void timer(long i);
+void * produce(void* args);
+void * consume(void * args);
